d25425c268efc8a63ffa84b25dc6bc5abf56eecf
Kumar vibhanshu <vibhanshumonty@gmail.com>
2021-08-11 17:51:54 +0530

    Initial Commit

---
+++ js/theme.js
@@ -0,0 +1,14 @@
+
+
+
+$(function(){
+   $('.menuIcon').click(function(){
+      $('.menuIcon i').toggleClass('fa-times-circles');
+      $('.headerMenu').toggleClass('active');
+    });
+
+    // $(".sub-btn").click(function(){
+    //   $(this).next(".sub-menu").slideToggle();
+    // });
+});
+
